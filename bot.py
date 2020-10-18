75a115256fe9d026d00468689d078ae72f297582
Freddie <freddieburns@hotmail.co.uk>
2020-10-18 18:13:09 +0100

    Update on_ready to print bot and guild names

--- bot.py
+++ bot.py
@@ -10,13 +10,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} has connected to the guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
