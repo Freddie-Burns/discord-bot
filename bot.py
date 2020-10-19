738daac18e4db335d3fee55a5746e8e891d55690
Freddie <freddieburns@hotmail.co.uk>
2020-10-19 12:04:30 +0100

    Use real discord token

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ load_dotenv(encoding='utf-8')
 DEATH_PROB = float(os.getenv('DEATH_PROB'))
 MAX_SLEEP = float(os.getenv('MAX_SLEEP'))
 SLEEP_STEP = float(os.getenv('SLEEP_STEP'))
-TOKEN = os.getenv('TEST_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 GOOD_EMOJIS = "🤩🥳😘🤗😲👽😼🎉🎀🎊"
 BAD_EMOJIS = "🙃🤬😭😤🥺😖😒🤯🥵🥶😱😳👹⛈💔❌⛔🔕"
